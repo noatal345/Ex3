f95bb39e83eef122a8930d53bdb27e026c86e4c7
oneOfThePeople <gogogta@gmail.com>
2023-06-11 14:31:56 +0300

    init_pop.py fix

--- init_pop.py
+++ init_pop.py
@@ -5,17 +5,15 @@ class model:
     def __init__(self, num_of_layers, num_of_neurons):
         self.num_of_layers = num_of_layers
         self.num_of_neurons = num_of_neurons
-        self.weights = np.array([])
-        self.biases = np.array([])
+        self.weights = []
+        self.biases = []
         self.fitness = 0
         self.init_weights()
 
     def init_weights(self):
         for i in range(self.num_of_layers - 1):
-            new_weights = np.random.uniform(-1, 1, (self.num_of_neurons[i], self.num_of_neurons[i + 1]))
-            new_biases = np.random.uniform(-1, 1, (1, self.num_of_neurons[i + 1]))
-            self.weights = np.concatenate((self.weights, new_weights.flatten()))
-            self.biases = np.concatenate((self.biases, new_biases.flatten()))
+            self.weights.append(np.random.randn(self.num_of_neurons[i], self.num_of_neurons[i + 1]))
+            self.biases.append(np.random.randn(self.num_of_neurons[i + 1])) # todo: maybe change shape
 
 
 def init_pop(pop_size, num_of_layers, num_of_neurons):
--- main.py
+++ main.py
@@ -1,1 +1,2 @@
 from init_pop import init_pop, model
+import numpy as np
